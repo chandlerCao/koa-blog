1f012dbcc1c147eceed7f9ac9b5a8d955c5fc251
chandler <597649635@qq.com>
2018-12-24 18:06:37 +0800

    update

--- index/controller/index.js
+++ index/controller/index.js
@@ -1,11 +1,16 @@
 const router = require('koa-router')();
 const articleModel = require('../model/articleModel');
+const fs = require('fs');
 // 获取文章模型
 const article = new articleModel();
 // 获取文章列表
+router.get('', async c => {
+    const indexPage = fs.readFileSync('../view/index.html').toString();
+    c.body = indexPage;
+});
 router.post('/article/getArticleList', async c => {
     const ip = c.req.connection.remoteAddress;
-    let { page } = c.request.body;
+    let { type, page } = c.request.body;
     if (page) page = parseInt(page);
     else page = 1;
     // 查询限制条数
@@ -13,7 +18,7 @@ router.post('/article/getArticleList', async c => {
     const skip = (page - 1) * articleLen;
     const data = {};
     // 文章列表
-    let articleList = await article.getArticleList(ip, skip, articleLen);
+    let articleList = await article.getArticleList(ip, type, skip, articleLen);
     articleList.map(articleList => {
         articleList.cover = `${c.domain}:${c.port}/${articleList.cover}`;
         articleList.tag_url = `${c.domain}:${c.port}/${c.icon_dir}/${articleList.tag_name}`;
@@ -21,12 +26,20 @@ router.post('/article/getArticleList', async c => {
     data.articleList = articleList;
 
     // 文章总数
-    let total = await article.getArticleTotal();
+    let total = await article.getArticleTotal(type);
     total = total[0].total;
     data.total = total;
 
     // 每页显示条数
     data.page_size = articleLen;
+
+    // await async function () {
+    //     return new Promise(resolve => {
+    //         setTimeout(() => {
+    //             resolve();
+    //         }, 1000);
+    //     })
+    // }();
 
     c.body = {
         code: 0,
@@ -36,6 +49,7 @@ router.post('/article/getArticleList', async c => {
 });
 // 根据id获取文章内容
 router.post('/article/getArticleCnt', async c => {
+    const ip = c.req.connection.remoteAddress;
     const { aid } = c.request.body;
     if (!aid) {
         c.body = {
@@ -47,10 +61,11 @@ router.post('/article/getArticleCnt', async c => {
     // 更新文章阅读数量
     const read_count = await article.getArticleReadCount(aid);
     await article.setArticleReadCount(aid, ++read_count[0].read_count);
-    const articleInfo = await article.getArticleCnt(aid);
+    const articleInfo = await article.getArticleCnt(aid, ip);
     if (articleInfo.length) {
         const articleContent = articleInfo[0];
         articleContent.cover = `${c.domain}:${c.port}/${articleContent.cover}`;
+        articleContent.tag_url = `${c.domain}:${c.port}/${c.icon_dir}/${articleContent.tag_name}`;
         c.body = {
             code: 0,
             articleContent,
@@ -75,14 +90,14 @@ router.post('/article/getArticleTag', async c => {
 // 通过标签获取文章列表
 router.post('/article/getArticleListByTag', async c => {
     const ip = c.req.connection.remoteAddress;
-    let { tid, page } = c.request.body;
+    let { tag, page } = c.request.body;
     // 查询限制条数
     const articleLen = c.articleLen;
     const skip = (page - 1) * articleLen;
     const data = {};
 
     // 文章列表
-    const articleList = await article.getArticleListByTag(ip, tid, skip, articleLen);
+    const articleList = await article.getArticleListByTag(ip, tag, skip, articleLen);
     articleList.map(articleList => {
         articleList.cover = `${c.domain}:${c.port}/${articleList.cover}`;
         articleList.tag_url = `${c.domain}:${c.port}/${c.icon_dir}/${articleList.tag_name}`;
@@ -90,7 +105,7 @@ router.post('/article/getArticleListByTag', async c => {
     data.articleList = articleList;
 
     // 文章总数
-    let total = await article.getArticleTotalByTid(tid);
+    let total = await article.getArticleTotalByTag(tag);
     total = total[0].total;
     data.total = total;
 
