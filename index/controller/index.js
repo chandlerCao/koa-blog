53df08cf5c0b8754fe3f20d77b9f24dc089cb357
chandler <597649635@qq.com>
2018-12-26 17:31:32 +0800

    删除querystring

--- index/controller/index.js
+++ index/controller/index.js
@@ -1,6 +1,5 @@
 const router = require('koa-router')();
 const articleModel = require('../model/articleModel');
-const querystring = require('querystring');
 const fs = require('fs');
 // 获取文章模型
 const article = new articleModel();
