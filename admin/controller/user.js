b0ef598ce6d5dfd80c7aac978854cb81b9f6acb6
chandlerCao <597649635@qq.com>
2020-07-02 17:45:46 +0800

    用户注册

--- admin/controller/user.js
+++ admin/controller/user.js
@@ -1,12 +1,16 @@
 const Router = require('koa-router');
+const path = require('path');
 const MD5 = require('md5.js');
 // 获取用户模型
 const UserModel = require('../model/UserModel');
 // 实例化用户模型
 const usermodel = new UserModel();
+const config = require('../../config');
 const user = new Router();
 const generateToken = require('../../middleware/token').generateToken;
 const randomID = require('../../utils/random-id');
+const dateDir = require('../../utils/date-dir');
+const koaBody = require('koa-body');
 // 管理员登录
 user.post('/user/login', async ctx => {
     let { username, password } = ctx.request.body;
@@ -46,8 +50,8 @@ user.post('/user/login', async ctx => {
                 c: 0,
                 m: '登录成功！',
                 d: {
-                    username: user.username,
-                    token: generateToken({ isAdmin: user.isAdmin, uid: user.uid, username: user.username })
+                    uid: user.uid,
+                    token: generateToken({ uid: user.uid, username: user.username, isAdmin: user.isAdmin })
                 }
             }
         } else {
@@ -63,10 +67,32 @@ user.post('/user/login', async ctx => {
         }
     }
 });
+// 用户头像上传
+user.post('/user/uploadUserAvatar', koaBody({
+    multipart: true,
+    formidable: {
+        uploadDir: path.join(config.root_dir, `${config.static_dir}`), // 设置文件上传目录
+        keepExtensions: true,    // 保持文件的后缀
+        maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
+        onFileBegin(name, file) {
+            file.path = `${this.uploadDir}/${dateDir()}/${randomID()}`; // 设置文件上传目录
+        }
+    }
+}), async ctx => {
+    const { image } = ctx.request.files;
+    let imgPath = image.path.split(ctx.state.static_dir)[1];
+    ctx.body = {
+        c: 0,
+        d: {
+            url: `${ctx.state.host}${imgPath}`,
+            m: '头像上传成功！'
+        }
+    }
+});
 // 用户注册
 user.post('/user/register', async ctx => {
     // 获取用户名和密码
-    const { username, password } = ctx.request.body;
+    let { username = '', password = '', password_confirm = '', avatar = '' } = ctx.request.body;
     if (username.trim() === '') {
         ctx.body = {
             c: 1,
@@ -81,6 +107,20 @@ user.post('/user/register', async ctx => {
         }
         return;
     }
+    if (password !== password_confirm) {
+        ctx.body = {
+            c: 1,
+            m: '两次输入密码不一致！',
+        }
+        return;
+    }
+    if (avatar.trim() === '') {
+        ctx.body = {
+            c: 1,
+            m: '请上传头像！',
+        }
+        return;
+    }
     // 检查是否已经存在
     const checkUserRes = await usermodel.checkUser(username)
     if (checkUserRes && checkUserRes.length) {
@@ -90,19 +130,38 @@ user.post('/user/register', async ctx => {
         }
     } else {
         // 随机id
-        const id = randomID();
+        const uid = randomID();
         // 注册
-        const res = await usermodel.register(id, username, password);
+        const md5stream = new MD5();
+        md5stream.end(password);
+        password = md5stream.read().toString('hex');
+        avatar = avatar.replace(new RegExp(`${ctx.state.host}\/`), '');
+        await usermodel.register(uid, username, password, avatar);
         ctx.body = {
             c: 0,
-            m: '注册成功！',
-            token: token({ uid: res.uid, username: res.username }, id)
+            d: {
+                uid,
+                token: generateToken({ uid, username, isAdmin: 1 })
+            },
+            m: '用户注册成功！'
         }
     }
 });
-user.post('/user/checkLogin', async ctx => {
+// 获取用户信息
+user.get('/user/getUserInfo', async ctx => {
+    const { uid = '' } = ctx.query
+    const userInfo = (await usermodel.getUserInfo(uid))[0]
+    if (!userInfo) {
+        ctx.body = {
+            c: 1,
+            m: '未查询到当前用户！'
+        }
+        return
+    }
+    userInfo.avatar = ctx.state.host + '/' + userInfo.avatar;
     ctx.body = {
-        c: 0
+        c: 0,
+        d: { ...userInfo }
     }
 });
-module.exports = user;
+module.exports = user
--- admin/model/Usermodel.js
+++ admin/model/Usermodel.js
@@ -1,22 +1,24 @@
 const db = require('../../db');
 class User {
     // 注册
-    async register(id, username, password) {
-        const sql = 'insert into user (uid, username, password) values (?, ?, ?);';
-        const values = [id, username, password];
-        return await db.query(sql, values);
+    async register(id, username, password, avatar) {
+        const sql = `insert into user (uid, username, password, isAdmin, avatar) values ('${id}', '${username}', '${password}', 1, '${avatar}')`;
+        return await db.query(sql);
     }
     // 登录
     async login(username, password) {
-        const sql = 'select * from user where username = ? and password = ?';
-        const values = [username, password];
-        return await db.query(sql, values);
+        const sql = `select * from user where username = '${username}' and password = '${password}'`;
+        return await db.query(sql);
     }
     // 检查用户是否存在
     async checkUser(username) {
-        const sql = 'select username from user where username = ?';
-        const values = [username];
-        return await db.query(sql, values);
+        const sql = `select username from user where username = '${username}'`;
+        return await db.query(sql);
+    }
+    // 获取用户信息
+    async getUserInfo(uid) {
+        const sql = `select * from user where uid = '${uid}'`;
+        return await db.query(sql)
     }
 }
 module.exports = User;
